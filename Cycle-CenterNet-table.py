c6fdbf07d7ba2db5d92313a42592655601414311
Herbert0305 <149051101+Herbert0305@users.noreply.github.com>
2024-05-08 15:04:57 +0800

    OCR

--- Cycle-CenterNet-table.py
+++
@@ -1,6 +0,0 @@
-#https://modelscope.cn/models/iic/cv_dla34_table-structure-recognition_cycle-centernet/summary
-from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
-table_recognition = pipeline(Tasks.table_recognition, model='damo/cv_dla34_table-structure-recognition_cycle-centernet')
-result = table_recognition('4.png')
-print(result)
